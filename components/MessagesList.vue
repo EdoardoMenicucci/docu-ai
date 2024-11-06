c8035aea3e8872799dfb9c0629d9d8643361fd27
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-11-06 19:43:23 +0100

    pinia storeToRefs

--- components/MessagesList.vue
+++ components/MessagesList.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- CHAT -->
-  <div v-if="chatStore.currentMessages" v-for="res in chatStore.currentMessages">
+  <div v-if="messages" v-for="res in messages" class="chat-area">
     <div class="flex mx-5" :class="{ 'justify-end': res.user === 'USER' }">
       <div class="w-3/4 relative text-white bg-dark-gray-900 p-5 rounded-xl border-dark-gray-500 border-2 m-4 order-1"
         :class="{ 'order-2': res.user === 'AI' }">
@@ -14,15 +14,31 @@
         v-if="res.user === 'USER'" size="2em" />
       <UIcon v-else name="mdi:robot-outline" class="text-dark-gray-200 order-1" mode="svg" size="2em" />
 
+
     </div>
+
   </div>
+  <UProgress v-if="isSendingMessages" animation="swing" class="w-3/4 mx-auto mb-5" />
 </template>
 
 <script lang="ts" setup>
 
 import { useChatStore } from '~/stores/useChatStore';
+import { storeToRefs } from 'pinia';
 
 const chatStore = useChatStore();
+const { messages, isSendingMessages } = storeToRefs(chatStore);
+
+// Osserva i cambiamenti nei messaggi della chat e aggiorna l'UI
+watch(
+  () => messages.value,
+  () => {
+    const chatArea = document.querySelector('.chat-area');
+    if (chatArea) {
+      chatArea.scrollTop = chatArea.scrollHeight;
+    }
+  }
+);
 
 </script>
 
--- components/PdfChatSection.vue
+++ components/PdfChatSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="chatStore.currentPdfUrl" class="mx-5 flex justify-end">
+  <div v-if="pdfUrl" class="mx-5 flex justify-end">
     <div class="mb-5 mx-5 mt-4 justify-end">
       <embed width="500" height="760" :src="chatStore.currentPdfUrl" type='application/pdf'></embed>
     </div>
@@ -9,7 +9,10 @@
 
 <script lang="ts" setup>
 import { useChatStore } from '~/stores/useChatStore';
+import { storeToRefs } from 'pinia';
 const chatStore = useChatStore();
+
+const { pdfUrl } = storeToRefs(chatStore);
 </script>
 
 <style></style>
